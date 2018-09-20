e34c66cdf1de43394b02c8664e1ca019fb322ce4
ThienVuLe <vule@gumiviet.com>
2018-09-20 21:16:58 +0700

    Initial Commit

---
+++ Youtube-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-clone
+//
+//  Created by Thien Vu Le on Sep/20/18.
+//  Copyright © 2018 Thien Vu Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
